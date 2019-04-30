bbafc04d043d6743a610762667f3b2b8f54c35a1
yduan004 <yduan004@ucr.edu>
2019-04-30 14:51:02 -0700

    feat: Query local SQLite
    User need to generate the DrugBank SQLite database locally and query it
    by providing path to the local SQLite db. Instead of downloading from
    biocluster to system directory, which will cause errors.

--- R/queries.R
+++ R/queries.R
@@ -1,34 +1,24 @@
 ## Get the entire data frame
 #' @import RSQLite
 #' @importFrom utils download.file
-getAll <- function(version="5.0.10"){
-  ext_path <- system.file("extdata", package="drugbankR")
-  db_path <- paste0(ext_path,"/drugbank_",version,".db")
-  if(file.exists(db_path)){
+getAll <- function(db_path){
     conn <- dbConnect(SQLite(), db_path)
-  } else {
-    tryCatch(download.file(paste0("http://biocluster.ucr.edu/~yduan004/drugbankR/drugbank_", version,".db"), db_path, quiet = TRUE), 
-             error = function(e){
-               stop("we currently don't have requested version of drugbank SQLite database. You could try version 5.0.10 or 5.0.6. If your version is more update, you could generate drugbank SQLite database by yourself by following the vignette of this package and then run this function")
-               })
-    conn <- dbConnect(SQLite(), db_path)
-  }
 	dbdf <- dbGetQuery(conn,'SELECT * FROM dbdf')
 	dbDisconnect(conn)
 	return(dbdf)
 }
 
 ## Get all the DrugBank IDs
-getIDs <- function(version="5.0.10"){
-	dbdf <- getAll(version)
+getIDs <- function(db_path){
+	dbdf <- getAll(db_path)
 	ids <- dbdf[,"drugbank-id"]
 	names(ids) <- dbdf[,"name"]
 	return(ids)
 } 
 
 ## To check wether the drug is FDA approved
-whichFDA <- function(ids, version="5.0.10"){
-	dbdf <- getAll(version)
+whichFDA <- function(ids, db_path){
+	dbdf <- getAll(db_path)
 	sub_dbdf <- dbdf[dbdf$"drugbank-id" %in% ids,]
 	whichFDA <- grepl("approved",sub_dbdf$groups)
 	res <- data.frame(drugbank_id = sub_dbdf$"drugbank-id", name=sub_dbdf$"name", whichFDA=whichFDA)
@@ -40,28 +30,15 @@ whichFDA <- function(ids, version="5.0.10"){
 #' @importFrom dplyr as_tibble
 #' @importFrom dplyr left_join
 #' @import AnnotationDbi
-getTargets <- function(ids, version="5.0.10"){
+getTargets <- function(ids, db_path){
 	len <- length(ids)
-	dbdf <- getAll(version)
+	dbdf <- getAll(db_path)
 	tartxt <- dbdf[dbdf$"drugbank-id" %in% ids,"targets"]
 	
 	ext_path <- system.file("extdata", package="drugbankR")
-	db_path <- paste0(ext_path,"/drugbank_",version,".db")
-	conn <- dbConnect(SQLite(), db_path)
-	
-	dt_path <- paste0(ext_path,"/drug_target_uniprot_links_",version,".csv")
-	if(file.exists(dt_path)){
-	  drug_target_uniprot <- read.csv(dt_path, stringsAsFactors = FALSE)
-	} else {
-	  vv <- unlist(strsplit(version,split="\\."))
-	  v2 <- paste0(vv, collapse = "-")
-	  system(paste0("curl -Lfv -o ",paste0(ext_path,"/tmp.zip")," -u yduan004@ucr.edu:dbpass123 https://www.drugbank.ca/releases/", v2,"/downloads/target-all-uniprot-links > /dev/null 2>&1"))
-	  utils::unzip(paste0(ext_path,"/tmp.zip"), exdir = ext_path)
-	  file.rename(paste0(ext_path,"/uniprot links.csv"), dt_path)
-	  unlink(paste0(ext_path,"/tmp.zip"))
-	  drug_target_uniprot <- read.csv(dt_path, stringsAsFactors = FALSE)
-	}
-	dbDisconnect(conn)
+	dt_path <- paste0(ext_path,"/drug_target_uniprot_links_5.1.2.csv")
+	drug_target_uniprot <- read.csv(dt_path, stringsAsFactors = FALSE)
+
 	tar_df <- as.data.frame(matrix(NA, nrow=len, ncol=5, dimnames=list(1:len,c("q_db_id", "t_db_id","t_Uni_id","t_Uni_name","t_gn_sym"))))
 	for(i in 1:len){
 		gregout <- gregexpr("BE[0-9]{7}",tartxt[i])
@@ -74,7 +51,7 @@ getTargets <- function(ids, version="5.0.10"){
 	k <- keys(org.Hs.eg.db, keytype = "UNIPROT")
 	uni_gnsym <- suppressMessages(AnnotationDbi::select(org.Hs.eg.db, keys=k, columns=c("UNIPROT", "SYMBOL"), keytype="UNIPROT"))
 	drug_target_uniprot <- dplyr::as_tibble(drug_target_uniprot)
-  uni_gnsym <- dplyr::as_tibble(uni_gnsym)
+    uni_gnsym <- dplyr::as_tibble(uni_gnsym)
 	dt_uni_sym <- dplyr::left_join(drug_target_uniprot, uni_gnsym, by = c("UniProt.ID"="UNIPROT"))
 	for(i in 1:len){
 		UniProt_id <- drug_target_uniprot[drug_target_uniprot$"DrugBank.ID"==ids[i],"UniProt.ID"][[1]]
@@ -104,9 +81,7 @@ getTargets <- function(ids, version="5.0.10"){
 	return(tar_df)
 }
 
-## Meta function
-#' @export
-#' @title Query drugbank SQLite database.
+#' @title Query DrugBank SQLite database.
 #' @description
 #' This function can be used to
 #' 
@@ -117,51 +92,65 @@ getTargets <- function(ids, version="5.0.10"){
 #' 3. given drugbank ids, determine whether the drugs are FDA approved
 #' 
 #' 4. given drugbank ids, get their targets ids (DrugBank_id, UniProt_id, symbol...).
+#' @details 
+#' Column description of the queryDB result when type is 'getTargets':
+#' \itemize{
+#'   \item q_db_id: DrugBank ids of the query drugs
+#'   \item t_db_id: DrugBank ids of the targets of the query drugs
+#'   \item t_Uni_id: Uniprot ids of the targets
+#'   \item t_Uni_name: Uniprot names of the targets
+#'   \item t_gn_sym: Gene SYMBOL of the target proteins
+#' }
 #' 
-#' @param ids A character vector, represent DrugBank IDs.
-#' @param type Type can only be assigned to "getAll","getIDs","whichFDA","getTargets" 
+#' @param ids character vector, represents DrugBank IDs.
+#' @param type one of "getAll","getIDs","whichFDA","getTargets" 
+#' \itemize{
+#'   \item getAll: get the entire drugbank dataframe, argument \code{ids} is ignored
 #' 
-#' getAll: get the entire drugbank dataframe, argument \code{ids} is ignored
+#'   \item getIDs: get all the drugbank ids, argument \code{ids} is ignored
 #' 
-#' getIDs: get all the drugbank ids, argument \code{ids} is ignored
+#'   \item whichFDA: given drugbank ids, determine whether they are FDA approved
 #' 
-#' whichFDA: given drugbank ids, determine whether they are FDA approved
+#'   \item getTargets: given drugbank ids, get their targets ids (DrugBank_id, UniProt_id, symbol...).
+#' }
 #' 
-#' getTargets: given drugbank ids, get their targets ids (DrugBank_id, UniProt_id, symbol...).
-#' 
-#' @param version Character, version of the requested drugbank database
+#' @param db_path Character(1), path to the DrugBank SQLite database generated
+#' from \code{\link{dbxml2df}} and \code{\link{df2SQLite}} function
 #' @return \code{getAll}: drugbank dataframe
 #' 
-#' \code{getIDs}: vector of all the drugbank ids
+#' \code{getIDs}: character vector of all the drugbank ids
 #' 
 #' \code{whichFDA}: logical vector
 #' 
-#' \code{getTargets}: dataframe containing targets' DrugBank_id, UniProt_id, UniProt_name and gene_symbol
+#' \code{getTargets}: data.frame containing DrugBank_id, UniProt_id, UniProt_name 
+#' and gene_symbol of the targets of the query drugs
+#' @export
 #' 
-queryDB <- function(ids=NULL, type, version="5.0.10"){
+queryDB <- function(ids=NULL, type, db_path){
   ids <- unique(ids)
-	## Validity check of input ids
-  val_ids <- suppressMessages(getIDs(version))
+  ## Validity check of input ids
+  val_ids <- suppressMessages(getIDs(db_path))
   ids_inval <- ids[! ids %in% val_ids]
   ids <- ids[ids %in% val_ids]
-	if(length(ids_inval)>0) message("No targets in DrugBank for", ids_inval)
+  if(length(ids_inval)>0) message("The following are not valid DrugBank ids: \n", 
+                                  paste(ids_inval, collapse=" "))
   if(length(ids)==0 & !is.null(ids)) stop("Couldn't find targets in DrugBank for your input ids")
   ## Validity check of type
   if(type=="getAll") {
-		if(!is.null(ids)) warning("Get the entire DrugBank database, argument ids is ignored")			
-		return(getAll(version))
+		if(!is.null(ids)) warning("Get the entire DrugBank database, argument 'ids' is ignored")			
+		return(getAll(db_path))
 	}
   if(type=="getIDs") {
-		if(!is.null(ids)) warning("Get all the DrugBank IDs, argument ids is ignored")
-		return(getIDs(version))
+		if(!is.null(ids)) warning("Get all the DrugBank IDs, argument 'ids' is ignored")
+		return(getIDs(db_path))
 	}
   if(type=="whichFDA"){
-		if(is.null(ids)) stop("Argument ids is not assigned")
-		return(whichFDA(ids, version)) 
+		if(is.null(ids)) stop("Argument 'ids' is not assigned")
+		return(whichFDA(ids, db_path)) 
 	}
   if(type=="getTargets"){
-		if(is.null(ids)) stop("Argument ids is not assigned")
-		return(getTargets(ids, version))
+		if(is.null(ids)) stop("Argument 'ids' is not assigned")
+		return(getTargets(ids, db_path))
 	} else stop("Argument type can only be assigned one of: getAll, getIDs, whichFDA or getTargets.")
 }
 
